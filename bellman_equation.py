7084d724a5591cba2477ddbdbdd2ebacd87f5324
Raúl Manzanero <74859614+RaulMLA@users.noreply.github.com>
2022-05-05 18:41:00 +0000

    Transition Function v4

---
+++ bellman_equation.py
@@ -0,0 +1,21 @@
+def bellman(s):
+    results = []
+    actions = ["N", "E", "W"]
+    actions_cost = {"N": , "E": , "W": }
+    for a in actions:
+        r = actions_cost[actions[a]] + x
+        append.results(r)
+    return results
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+slou mou
+------
+#poner bien
+dict1 = 0
+dict2 = 0
+i = 0
+condition = False
+while not condition:
+    for i in range(len(dict1)):
+        
+        
--- probabilty_calculation.py
+++ probabilty_calculation.py
@@ -38,14 +38,15 @@ def CalculateProbabilities():
     # Matrix which stores the data of the csv file.
     data = []
 
-    # Total probabilities must be NUM_ROWS * NUM_COLUMNS (192 for the 3 intersection model).
-    total_probabilities = 0
+    # Total probabilities is the number of rows of the file.
+    total_rows = 0
     
     # Data extraction from the csv file.
     with open(DIR_PATH, newline = '') as File:
         reader = csv.reader(File, delimiter = ";")
         for row in reader:
-            if total_probabilities != 0:
+            # We skip the first row (header with titles).
+            if total_rows != 0:
                 new_row = []
                 # e.g., row[0][0] takes the first letter (H or L) of the first field of the row (Initial traffic level N).
                 initial_traffic = row[0][0] + row[1][0] + row[2][0]
@@ -58,19 +59,19 @@ def CalculateProbabilities():
 
                 '''
                 # Information for debugging.
-                print('[\nROW %d]' %i)
-                print('Initial traffic level N: ', row[0])
-                print('Initial traffic level E: ', row[1])
-                print('Initial traffic level W: ', row[2])
+                #print('[\nROW %d]' %i)
+                print('Initial traffic level N: ', row[0][0])
+                print('Initial traffic level E: ', row[1][0])
+                print('Initial traffic level W: ', row[2][0])
                 print('Green traffic light: ', row[3])
-                print('Final traffic level N: ', row[4])
-                print('Final traffic level E: {1}', row[5])
-                print('Final traffic level W: {1}', row[6])
+                print('Final traffic level N: ', row[4][0])
+                print('Final traffic level E: ', row[5][0])
+                print('Final traffic level W: ', row[6][0])
                 '''
 
-            total_probabilities += 1
+            total_rows += 1
 
-    total_probabilities -= 1
+    total_rows -= 1
 
     '''
     # Print the matrix with all the data.
@@ -111,8 +112,8 @@ def CalculateProbabilities():
                                 counts.append(count)
 
                                 # Testing data into a dictionary with the corresponding keys.
-                                key = STATES[initial_state_1] + STATES[initial_state_2] + STATES[initial_state_3] + '/' \
-                                + ACTIONS[action] + ',' + STATES[goal_state_1] + STATES[goal_state_2] + STATES[goal_state_3]
+                                key = STATES[goal_state_1] + STATES[goal_state_2] + STATES[goal_state_3] + '/' \
+                                + ACTIONS[action] + ',' + STATES[initial_state_1] + STATES[initial_state_2] + STATES[initial_state_3]
                                 data_dictionary[key] = count
                                 
                                 '''
@@ -127,25 +128,15 @@ def CalculateProbabilities():
                                 #print('List [{0}]: {1}' .format(i + 1, data[i]))
                                 '''
 
-
     '''
-    # Check if the number of items counted is OK.
-    count = 0
-    for item in counts:
-        count += item
-    print(counts)
-    print('Expected count: {0}\nReal count: {1}' .format(total_probabilities, count))
-    '''
-
     print('\n\n\nCount of rows from the csv file (using a dictionary):')
     for item in data_dictionary:
         print('Count({0}) = {1}'.format(item, data_dictionary[item]))
+    '''
 
     # Separation of counts into different lists to form a matrix (probabilities).
     counter = NUM_COLUMNS
     new_row = []
-    probabilities = []
-
 
     for count in counts:
         if counter != 0:
@@ -186,38 +177,6 @@ def CalculateProbabilities():
     print('\n\n\nTransitions matrix (Probabilities):')
     for row in matrix:
         print(row)
-
-
-    '''
-    # Matrix (nested lists) where i are the rows (24 possible) and j the columns (8 possible).
-    probabilities = []
-
-    # Initialization of the matrix of probabilities to 0.
-    for i in range(24):
-        probabilities.append([])
-        for j in range(8):
-            probabilities[i][j] = 0
-
-
-    # Another way would be define variables (inefficcient).
-    hhh_e_hhh = 0
-    hhl_e_hhh = 0
-    hlh_e_hhh = 0
-    hll_e_hhh = 0
-
-    for i in range(len(data)):
-        for j in range(7):
-            if data[i][0] == 'High' and data[i][1] == 'High' and data[i][2] == 'High' and data[i][3] == 'N' and data[i][4] == 'High' and data[i][5] == 'High' and data[i][6] == 'High':
-                hhh_e_hhh += 1
-            elif data[i][0] == 'High' and data[i][1] == 'High' and data[i][2] == 'Low' and data[i][3] == 'N' and data[i][4] == 'High' and data[i][5] == 'High' and data[i][6] == 'High':
-                hhl_e_hhh += 1
-            elif data[i][0] == 'High' and data[i][1] == 'Low' and data[i][2] == 'High' and data[i][3] == 'N' and data[i][4] == 'High' and data[i][5] == 'High' and data[i][6] == 'High':
-                hlh_e_hhh += 1
-            elif data[i][0] == 'High' and data[i][1] == 'Low' and data[i][2] == 'Low' and data[i][3] == 'N' and data[i][4] == 'High' and data[i][5] == 'High' and data[i][6] == 'High':
-                hll_e_hhh += 1
-
-    print(round((hhh_e_hhh/counter), 6))
-    '''
 
     # Return the set of states, the set of actions and the set of probabilities.
     return STATES, ACTIONS, probabilities
