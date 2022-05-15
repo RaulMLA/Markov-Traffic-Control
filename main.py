44b81e21458b82524ecdc37a768174a1efae6c0c
Raúl Manzanero <74859614+RaulMLA@users.noreply.github.com>
2022-05-15 16:04:59 +0000

    Final Version v1

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ Note that COSTS keys must coincide with ACTIONS elements.
 STATES = ['H', 'L']
 ACTIONS = ['N', 'E', 'W']
 GOAL_STATES = ['LLL']
-COSTS = {'N': 1, 'E': 1, 'W': 1}
+COSTS = {'N': 20, 'E': 20, 'W': 20}
 
 # Constant string representing the name of the csv file with the data.
 FILE = "/Data.csv"
